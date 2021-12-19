87e4c53912d7caffbd394ecef1ac0a851c43d4c5
dgl-dev <les@dunawaygroupllc.com>
2021-12-18 19:28:59 -0500

    Added config.py, base.html, login screen and ...

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ __init__.py tells the world this is a package ANF
 gets the next made at startup
 """
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
